4b3aa710275c7657a813accb3e533049b0ed8ef0
Siddhant J <sidbnjadhav@gmail.com>
2023-08-04 21:34:36 +0530

    live streaming application update

--- video_application.cpp
+++ video_application.cpp
@@ -1,15 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+//user with the attributes. 
+class User{
+    string id;
+    string name;
+    string email;
+
+    public: 
+        string getID(){
+            return id;
+        }
+};
+
+class watchedVideo{
+
+    string id;
+    string videoId;
+    string userId;
+    int seekTime;
+
+    public: 
+
+    int getSeekTime(){
+        return seekTime;
+    }
+};
+class FileSystem{
+    public: 
+    Frame getVideo(string videoId){
+        return;
+    }
+};
+class VideoService{
+
+    private: 
+        FileSystem filesystem;
+    
+    public:
+        Frame getFrame(string videoId, int timestamp){
+            Video video  = FileSystem.getVideo(videoId);
+            return video.getFrame(timestamp);
+        }
+};
+
+
+class Database{
+
+    public: 
+    
+        watchedVideo getWatchedVideo(string userId, string videoId){
+            return;
+        }
+};
 class VideoConsumingServices{
 
+    private: 
+
+    Database database; 
+    public: 
+
+    int seekTime(string userId, string videoId){
+        watchedVideo watchedVideo = database.getWatchedVideo(userId,videoId);
+    }   
 
 };
 
 class Frame{
     public: 
     vector<int> bytes; //bytes array stored inside frames.
-    int timestamp;  //timestamp of the video. 
+    int timestamp;  //timestamp of the video.
+    int startTimestamp; //this is duration of frame-time.
+    int endTimestamp; 
 };
 
 
@@ -27,8 +90,8 @@ class Video{
     Frame getFrame(int timestamp){
         for(int i=0;i<frames.size();i++){
             //check if the 
-            if(frames[i].timestamp <= timestamp && (frames[i].timestamp+10) >= timestamp){
-                
+            if(frames[i].startTimestamp <= timestamp && frames[i].endTimestamp >= timestamp){
+                return frames[i];
             }
         }
     }
@@ -36,8 +99,3 @@ class Video{
 };
 
 
-class User{
-    string id;
-    string name;
-    string email;
-};
